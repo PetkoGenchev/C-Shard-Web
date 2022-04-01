d0a67a21c6405cd657e69373286cae64df7f31d7
PetkoGenchev <petko.genchev@yahoo.com>
2022-04-01 15:05:56 +0300

    Web-Server-01

---
+++ WebServer/Web-HTTP_Protocol/Controllers/CatsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_HTTP_Protocol.Controllers
+{
+
+    //  /cats/list
+    //  /cats/details
+    public class CatsController : Controller
+    {
+        public IActionResult List()
+        {
+
+            return View();
+
+            //this.Response.Headers.Add("Content-Disposition", "attachment");
+            //return File("/path/to/pdf", "application/pdf");
+
+
+            //return Redirect("/cats/search");
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        public IActionResult Search()
+        {
+            return View();
+        }
+    }
+}
